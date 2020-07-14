59655d00a7319067ca8faa1feafece3b00a58cc1
Gerard C. Saperas <gcsaperas@gmail.com>
2020-07-14 08:33:19 +0200

    Added process.env.MY_MAIL variable

--- server.js
+++ server.js
@@ -17,21 +17,21 @@ app.post('/send-email', (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'gcsaperas@gmail.com',
-            pass: 'faqawaqbjmtbzsbk'
+            user: process.env.MY_MAIL,
+            pass: process.env.MAIL_PASS
         },
         tls: { rejectUnauthorized: false }
     });
 
     const mailForMe = {
-        from: 'gcsaperas@gmail.com',
-        to: 'gcsaperas@gmail.com',
+        from: process.env.MY_MAIL,
+        to: process.env.MY_MAIL,
         subject: 'Web Dev - New Message',
         html: `<p>Hola a mi mateix,<br><br>Tinc un nou missatge.<br><br><b>De:</b> ${name}<br><br><b>email:</b> ${email}<br><br><b>Missatge:</b> ${comments}<br><br>Rebut a través de www.gerardcsaperas.com`
     };
 
     const mailForCustomer = {
-        from: 'gcsaperas@gmail.com',
+        from: process.env.MY_MAIL,
         to: `${email}`,
         subject: 'GerardCSaperas - Message Confirmation',
         html: `<p>Dear ${name},<br><br>Your message has been sent.<br><br><b>email:</b> ${email}<br><br><b>Message:</b> ${comments}<br><br>Thank you so much for your message, I'll get back to you as soon as possible.<br><br>Kind Regards,<br>Gerard</p>`
@@ -56,5 +56,5 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 465;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Express server is running on ' + PORT));
