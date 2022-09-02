7ca17ec8ad82d187ffac708defab860b06d85434
Gerard C. Saperas <60043225+gerardcsaperas@users.noreply.github.com>
2022-09-02 16:06:10 +0200

    about me

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,7 +1,61 @@
 import React from 'react';
+import Image from 'next/image';
+import { FaDownload } from 'react-icons/fa';
 
 const AboutMe = () => {
-    return <div>AboutMe</div>;
+    return (
+        <div id="about" className="bg-gray-700 h-96 flex">
+            <div className="py-14 pl-32">
+                <Image
+                    className="rounded-full"
+                    src="/avatar.jpeg"
+                    width="200"
+                    height="200"
+                ></Image>
+            </div>
+            <div className="py-10 px-20 text-gray-400 leading-7">
+                <h2 className="text-white font-bold my-5">About Me</h2>
+                <p>
+                    My name is Gerard, I'm a Full Stack Developer. Currently I'm
+                    working at Konodrac while also developing some freelance and
+                    personal projects.
+                </p>
+                <div className="flex space-x-20">
+                    <div>
+                        <h2 className="text-white font-bold my-5">
+                            Contact Details
+                        </h2>
+                        <div>
+                            <p>Gerard Castrelo Saperas</p>
+                            <p>+34 667 717 567</p>
+                            <p>gcsaperas@gmail.com</p>
+                        </div>
+                    </div>
+                    <div>
+                        <button
+                            className="
+                                bg-gray-500
+                                font-semibold
+                                text-xs
+                                lg:text-base
+                                text-white
+                                p-3
+                                mt-12
+                                rounded-sm
+                                hover:text-gray-500
+                                hover:bg-white
+                            "
+                        >
+                            <div className="flex items-center space-x-2">
+                                <FaDownload />
+                                <p>Download Resume</p>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default AboutMe;
