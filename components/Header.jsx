f4b7787c61a4d5a8f61c30ecd53f3cac04a37d9b
Gerard C. Saperas <60043225+gerardcsaperas@users.noreply.github.com>
2022-09-04 14:42:43 +0200

    animated scroll

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,41 @@
 import React from 'react';
-import Link from 'next/link';
+import { Link } from 'react-scroll';
+
 const Header = () => {
     return (
         <div className="fixed top-0 w-full z-10 p-2 bg-gray-800 flex justify-center space-x-5 text-white text-xs md:text-sm font-medium">
-            <Link href="/#home">HOME</Link>
-            <Link href="/#about">ABOUT</Link>
-            <Link href="/#resume">RESUME</Link>
-            <Link href="/#testimonials">TESTIMONIALS</Link>
-            <Link href="/#contact">CONTACT</Link>
+            <Link
+                className="cursor-pointer"
+                to="home"
+                smooth={true}
+                duration={500}
+            >
+                HOME
+            </Link>
+            <Link
+                className="cursor-pointer"
+                to="about"
+                smooth={true}
+                duration={500}
+            >
+                ABOUT
+            </Link>
+            <Link
+                className="cursor-pointer"
+                to="resume"
+                smooth={true}
+                duration={500}
+            >
+                RESUME
+            </Link>
+            <Link
+                className="cursor-pointer"
+                to="projects"
+                smooth={true}
+                duration={500}
+            >
+                PROJECTS
+            </Link>
         </div>
     );
 };
