58d3b2166ffdd7979a04df9119f2701287133380
Gerard C. Saperas <60043225+gerardcsaperas@users.noreply.github.com>
2022-09-04 14:43:29 +0200

    project spotlight

--- components/ProjectSpotlight.jsx
+++ components/ProjectSpotlight.jsx
@@ -1,7 +1,50 @@
 import React from 'react';
-
+import Image from 'next/image';
+import LinkToProject from './LinkToProject';
 const ProjectSpotlight = () => {
-    return <div>ProjectSpotlight</div>;
+    return (
+        <div className="grid grid-cols-1 xl:grid-cols-3">
+            <div className="relative text-center my-7">
+                <LinkToProject
+                    live="https://amazon-clone-swart-pi.vercel.app/"
+                    github="https://github.com/gerardcsaperas/amazon-clone"
+                    name="Amazon Clone"
+                />
+                <Image
+                    src="/amazon-clone.png"
+                    width="500"
+                    height="300"
+                    objectFit="contain"
+                />
+            </div>
+            <div className="relative text-center my-7">
+                <LinkToProject
+                    live="https://linkedin-clone-gerard.web.app/"
+                    github="https://github.com/gerardcsaperas/linkedin-clone"
+                    name="LinkedIn Clone"
+                />
+                <Image
+                    src="/linkedin-clone.png"
+                    width="500"
+                    height="300"
+                    objectFit="contain"
+                />
+            </div>
+            <div className="relative text-center my-7">
+                <LinkToProject
+                    live="https://www.cenandoenjapon.com"
+                    github="https://github.com/gerardcsaperas/cenandoenjapon"
+                    name="NextJS MDX Blog"
+                />
+                <Image
+                    src="/cenandoenjapon.png"
+                    width="500"
+                    height="300"
+                    objectFit="contain"
+                />
+            </div>
+        </div>
+    );
 };
 
 export default ProjectSpotlight;
