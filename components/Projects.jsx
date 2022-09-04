2ca73d3468d4db94e355c7f3b34b8f6c838d44dc
Gerard C. Saperas <60043225+gerardcsaperas@users.noreply.github.com>
2022-09-04 14:43:17 +0200

    add id for navigation

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ProjectSpotlight from './ProjectSpotlight';
 const Projects = () => {
     return (
-        <div className="bg-slate-100 p-10">
+        <div id="projects" className="bg-slate-100 p-10 pb-40">
             <h2 className="text-center text-sm tracking-widest font-semibold text-gray-400 my-20">
                 CHECK OUT SOME OF MY PROJECTS
             </h2>
